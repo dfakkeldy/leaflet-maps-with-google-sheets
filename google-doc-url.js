806f2335a47be21eb1fc2b1c950be9dd7f769a38
dfakkeldy <2112591+dfakkeldy@users.noreply.github.com>
2017-07-29 09:38:07 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1DmskEr4arpsYDk9YiQIWKKEtTwhSP2ALpMPHPJYbZBo/edit#gid=164271551';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1DmskEr4arpsYDk9YiQIWKKEtTwhSP2ALpMPHPJYbZBo/edit#gid=0';
